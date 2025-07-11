bdfea7d253d16f6ae6edd675f3d0abda036852aa
Shanidhya01 <luckykumar0011s@gmail.com>
2025-07-11 18:25:48 +0530

    Initial commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+          <DiCssdeck size="3rem" /> <span>Shanidhya Kumar</span>
         </a>
       </Link>
     </Div1>
